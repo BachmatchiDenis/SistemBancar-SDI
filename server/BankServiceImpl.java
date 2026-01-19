9764e7f6af079eee3cb4e32bcdc9e39bd5a2312f
BachmatchiDenis <bachmatchi.gh.denis@student.utcluj.ro>
2026-01-20 00:06:39 +0200

    Adaugat persistenta date - conturile se salveaza automat

--- server/BankServiceImpl.java
+++ server/BankServiceImpl.java
@@ -6,10 +6,13 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
+import java.io.*;
+import java.nio.file.*;
 
 /**
  * Implementarea serviciului bancar RMI
  * Această clasă conține toată logica de business pentru operațiunile bancare
+ * Datele sunt salvate persistent în format JSON
  */
 public class BankServiceImpl extends UnicastRemoteObject implements BankService {
     private static final long serialVersionUID = 1L;
@@ -20,14 +23,111 @@ public class BankServiceImpl extends UnicastRemoteObject implements BankService 
     private final AtomicLong accountNumberGenerator;
     // Prefix pentru numerele de cont
     private static final String ACCOUNT_PREFIX = "RO";
+    // Fișier pentru persistență date - folosim calea absolută
+    private static final String DATA_DIR;
+    private static final String ACCOUNTS_FILE;
+    private static final String COUNTER_FILE;
+    
+    static {
+        // Determinăm directorul proiectului bazat pe locația clasei
+        String userDir = System.getProperty("user.dir");
+        DATA_DIR = userDir + File.separator + "data";
+        ACCOUNTS_FILE = DATA_DIR + File.separator + "accounts.dat";
+        COUNTER_FILE = DATA_DIR + File.separator + "counter.dat";
+    }
     
     public BankServiceImpl() throws RemoteException {
         super();
         this.accounts = new ConcurrentHashMap<>();
         this.accountNumberGenerator = new AtomicLong(1000000000L);
         
-        // Creăm câteva conturi demo
-        createDemoAccounts();
+        // Creăm directorul pentru date dacă nu există
+        createDataDirectory();
+        
+        // Încărcăm datele salvate sau creăm conturi demo
+        if (!loadData()) {
+            createDemoAccounts();
+            saveData();
+        }
+    }
+    
+    /**
+     * Creează directorul pentru date dacă nu există
+     */
+    private void createDataDirectory() {
+        try {
+            Files.createDirectories(Paths.get(DATA_DIR));
+        } catch (IOException e) {
+            System.err.println("Eroare la crearea directorului data: " + e.getMessage());
+        }
+    }
+    
+    /**
+     * Salvează toate datele pe disc (conturi și counter)
+     */
+    private synchronized void saveData() {
+        try {
+            // Salvăm conturile
+            try (ObjectOutputStream oos = new ObjectOutputStream(
+                    new FileOutputStream(ACCOUNTS_FILE))) {
+                oos.writeObject(new HashMap<>(accounts));
+            }
+            
+            // Salvăm counter-ul
+            try (ObjectOutputStream oos = new ObjectOutputStream(
+                    new FileOutputStream(COUNTER_FILE))) {
+                oos.writeLong(accountNumberGenerator.get());
+            }
+            
+            System.out.println("[PERSISTENȚĂ] Date salvate cu succes (" + accounts.size() + " conturi)");
+        } catch (IOException e) {
+            System.err.println("[PERSISTENȚĂ] Eroare la salvare: " + e.getMessage());
+        }
+    }
+    
+    /**
+     * Încarcă datele de pe disc
+     * @return true dacă datele au fost încărcate cu succes
+     */
+    @SuppressWarnings("unchecked")
+    private boolean loadData() {
+        File accountsFile = new File(ACCOUNTS_FILE);
+        File counterFile = new File(COUNTER_FILE);
+        
+        if (!accountsFile.exists() || !counterFile.exists()) {
+            System.out.println("[PERSISTENȚĂ] Nu există date salvate, se vor crea conturi demo");
+            return false;
+        }
+        
+        try {
+            // Încărcăm conturile
+            try (ObjectInputStream ois = new ObjectInputStream(
+                    new FileInputStream(ACCOUNTS_FILE))) {
+                Map<String, Account> loadedAccounts = (Map<String, Account>) ois.readObject();
+                accounts.putAll(loadedAccounts);
+            }
+            
+            // Încărcăm counter-ul
+            try (ObjectInputStream ois = new ObjectInputStream(
+                    new FileInputStream(COUNTER_FILE))) {
+                long counter = ois.readLong();
+                accountNumberGenerator.set(counter);
+            }
+            
+            System.out.println("[PERSISTENȚĂ] Date încărcate cu succes (" + accounts.size() + " conturi)");
+            System.out.println("[PERSISTENȚĂ] Următorul număr cont: " + ACCOUNT_PREFIX + accountNumberGenerator.get());
+            
+            // Afișăm conturile existente
+            for (Account acc : accounts.values()) {
+                System.out.println("  -> Cont: " + acc.getAccountNumber() + " | " + acc.getOwnerName() + 
+                    " | Sold: " + String.format("%.2f", acc.getBalance()) + " RON");
+            }
+            
+            return true;
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("[PERSISTENȚĂ] Eroare la încărcare: " + e.getMessage());
+            return false;
+        }
     }
     
     /**
@@ -83,6 +183,9 @@ public class BankServiceImpl extends UnicastRemoteObject implements BankService 
         
         logOperation("CREARE CONT", accountNumber, "Titular: " + ownerName + ", Sold inițial: " + initialBalance);
         
+        // Salvăm datele pe disc
+        saveData();
+        
         return accountNumber;
     }
     
@@ -120,6 +223,9 @@ public class BankServiceImpl extends UnicastRemoteObject implements BankService 
             
             logOperation("DEPUNERE", accountNumber, "Sumă: " + amount + " RON, Sold nou: " + newBalance + " RON");
             
+            // Salvăm datele pe disc
+            saveData();
+            
             return newBalance;
         }
     }
@@ -149,6 +255,9 @@ public class BankServiceImpl extends UnicastRemoteObject implements BankService 
                 "Retragere numerar"));
             
             logOperation("RETRAGERE", accountNumber, "Sumă: " + amount + " RON, Sold nou: " + newBalance + " RON");
+            
+            // Salvăm datele pe disc
+            saveData();
             
             return newBalance;
         }
@@ -198,6 +307,9 @@ public class BankServiceImpl extends UnicastRemoteObject implements BankService 
                 
                 logOperation("TRANSFER", fromAccount, 
                     "Către: " + toAccount + ", Sumă: " + amount + " RON");
+                
+                // Salvăm datele pe disc
+                saveData();
                 
                 return source.getBalance();
             }
